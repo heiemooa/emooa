ef3a1f340a8778668f9b4887004b4401ed5c6381
heiemooa <243973775@qq.com>
2024-06-15 10:49:35 +0800

    feat: add ui local vite server

---
+++ packages/ui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from './containers/Image';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Image />
+    </>
+  );
+}
+
+export default App;
